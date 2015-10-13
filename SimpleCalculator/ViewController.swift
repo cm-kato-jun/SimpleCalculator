4fc9da4f21e6387255270fb61d4828ac92516665
kato jun <kato.jun@katojunnomacbook-air.local>
2015-10-13 12:33:19 +0900

    メソッド作成（実装はまだ。）

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -10,6 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func userDidTap0Button(sender: UIButton) {
+    }
+    @IBAction func userDidTap1Button(sender: UIButton) {
+    }
+    @IBAction func userDidTap2Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap3Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap4button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap5Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap6Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap7Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap8Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTap9Button(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapDotButton(sender: AnyObject) {
+    }
+    
+    @IBAction func userDidTapEqualButton(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapDeleteButton(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapDivisionButton(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapMultiplication(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapSubtractionButton(sender: UIButton) {
+    }
+    
+    @IBAction func userDidTapAdditionButton(sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +70,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
